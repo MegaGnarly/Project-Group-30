07915e223230802cc5506b745eb7a4ac6cbb9b4e
sahil <49390281+stndn0@users.noreply.github.com>
2022-04-19 14:02:43 +1000

    hardcoded patient and clinician identities. clean up app.js

--- app.js
+++ app.js
@@ -1,80 +1,86 @@
-// Import express and set server port
+// Set server port and import required modules
+const PORT = 3000;
 const express = require('express');
 const bodyParser = require('body-parser')
 const app = express();
-const PORT = 3000;
 const exphbs = require('express-handlebars');
 
-// Database schemas
+// Load database schemas
 const measuredValue = require('./models/measuredValue');
 const patient = require('./models/patient')
-
 require('./models')
-
-// Lines that configure handlebars
-app.engine('hbs', exphbs.engine({
-    defaultlayout: 'main',
-    extname: 'hbs'
-}))
-
-app.set('view engine', 'hbs')           // Set handlebars view engine
-app.use(express.static('public'))       // Define where static assets live
-
-// Used to expose body section for POST method
-app.use(bodyParser.urlencoded({ extended: false }));
-
-// Debugging middleware to log a message each time a request arrives at the server - handy for debugging
-app.use((req,res,next) => {
-    console.log('Message arrived: ' + req.method + ' ' + req.path)
-    next()
-})
 
 // link to our routers
 const appRouter = require('./routes/appRouter')
 const patientRouter = require('./routes/patientRouter')
 
-// the demo routes are added to the end of the '/test' path
+// Routing - set paths
 app.use('/test', appRouter)
-
 app.use('/patient', patientRouter)
 
-// Tells the app to send the string: "Our demo app is working!" when you hit the '/' endpoint.
+// Configure handlebars
+app.engine('hbs', exphbs.engine({
+    defaultlayout: 'main',
+    extname: 'hbs'
+}))
+
+// Set handlebars view engine
+app.set('view engine', 'hbs')           
+
+// Define where static assets live
+app.use(express.static('public'))       
+
+// Used to expose body section for POST method
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Debugging middleware that prints when a request arrives at the server 
+app.use((req,res,next) => {
+    console.log('Message arrived: ' + req.method + ' ' + req.path)
+    next()
+})
+
+
+// **** Application Endpoints ****  
+// Endpoint: site index
 app.get('/', (req,res) => {
     // Tip: you don't need to specify the .hbs extension
     console.log('SERVER: GET homepage')
     res.render('about_diabetes.hbs', {layout: 'main2'})
 })
 
-// Set some test routes. IMPORTANT: WE SHOULD MOVE THIS TO THE ROUTES FOLDER
+// Set some test routes. 
+// TODO: We should move these to the routes folder.
 app.get('/about_diabetes', (req,res) => {
     // Tip: you don't need to specify the .hbs extension
     console.log('SERVER: GET about_diabetes')
     res.render('about_diabetes.hbs', {layout: 'main2'})
 })
 
+
 app.get('/about_site', (req,res) => {
     res.render('about_site.hbs', {layout: 'main2'})
 })
+
 
 app.get('/test_data', (req,res) => {
     res.render('test_data.hbs')
 })
 
+
 app.get('/patient_dash', (req,res) => {
     // NOTE - As per the spec sheet, names are to be hard coded for this deliverable.
-    let userName = "Pat"
-    let userRole = "USER"
-    res.render('patient_dashboard.hbs', {userName:userName, userRole: userRole})
+    res.render('patient_dashboard.hbs', {userName:"Pat", userRole: "USER"})
 })
+
 
 app.get('/clinician_dash', (req,res) => {
     // NOTE - As per the spec sheet, names are to be hard coded for this deliverable.
-    let userName = "Chris"
-    let userRole = "CLINICIAN"
-    res.render('clinician_dashboard.hbs', {userName:userName, userRole: userRole})
+    res.render('clinician_dashboard.hbs', {userName:"Chris", userRole: "CLINICIAN"})
 })
 
-// sending blood glucose 
+
+// **** Application POSTs ****  
+// POST test - when the user fills the form, update the database.
 app.post('/post_values', (req,res) => {
     console.log('SERVER: New POST')
     let newValue = new measuredValue({
@@ -85,25 +91,24 @@ app.post('/post_values', (req,res) => {
     res.redirect('/test')
 })
 
-// For testing purposes - give the new user an ID
-// Note: should use the objectID that mongoDB generates instead but not sure how to access it in JS
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
-  }
-// user account creation
+
+// POST test - when the user creates an account, update the database.
 app.post('/post_values_user', (req,res) => {
     console.log('SERVER: New POST (acc creation)')
     let newValue = new patient({
         firstName: req.body.firstName,
         lastName: req.body.lastName,
-        id: getRandomInt(1000)
+        // Generate a random ID for the user. Ideally we should be using the ID that mongoDB generates.
+        id: Math.floor(Math.random() * 1000)
     })
     newValue.save()
     res.redirect('/test/users')
 })
 
 
+// Code to establish the server. 
 app.listen(process.env.PORT || PORT, () => {
-    console.log('Diabetes@Home is running!')
+    console.log('Diabetes@Home is running! CTRL + Click the URL below to open a browser window. Press CTRL + C to shut down the server.')
     console.log('http://127.0.0.1:' + PORT + '/' + '\n')
 })
+
