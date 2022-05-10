e1428c8716bce97bfe35e4ead4e38031f2bb6592
sahil <49390281+stndn0@users.noreply.github.com>
2022-05-10 14:37:30 +1000

    mostly finished with time series pop-up window. needs to be hooked to backend

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,10 +1,11 @@
+// console.log("SCRIPT LOADED")
+
 /*
 Summary:
 - Right now this code is responsible for making the hamburger menu functional/dynamic.
 - When a user clicks the hamburger icon, the following code will activate certain CSS classes
   that will bring up the sidebar.
 */
-
 // 'Hamburger' is the hamburger icon. Hamburger links are text within the sidebar.
 const hamburger = document.querySelector(".flexbox-navbar-hamburger")
 // const hamburgerLinks = document.querySelector(".flexbox-navbar-text")
@@ -29,3 +30,42 @@ closeBtn.addEventListener("click", () => {
     sidebar.classList.toggle("active");
 })
 
+
+// --------------------- For popup window in patient specifics ----------------------
+// These buttons are used to activate the popup menu and the dark backgroundMasker behind it.
+const openPopupButtons = document.querySelectorAll('[data-modal-target]')
+const closePopupButtons = document.querySelectorAll('[data-close-button]')
+const backgroundMasker = document.getElementById('backgroundMasker')
+
+openPopupButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = document.querySelector(button.dataset.modalTarget)
+    openModal(modal)
+  })
+})
+
+backgroundMasker.addEventListener('click', () => {
+  const modals = document.querySelectorAll('.modal.active')
+  modals.forEach(modal => {
+    closeModal(modal)
+  })
+})
+
+closePopupButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal')
+    closeModal(modal)
+  })
+})
+
+function openModal(modal) {
+  if (modal == null) return
+  modal.classList.add('active')
+  backgroundMasker.classList.add('active')
+}
+
+function closeModal(modal) {
+  if (modal == null) return
+  modal.classList.remove('active')
+  backgroundMasker.classList.remove('active')
+}
